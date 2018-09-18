6cd057d7661ab24b006e27b41bce6e9a3694a385
Alexander Jaroszewicz <alex.jaro@au1.ibm.com>
2018-09-18 13:59:44 +1000

    update the gravity to support jumping from the platform

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 // use for offline development
 // import 'phaser'
 
-import Example from './scenes/Example'
-
+import Example from "./scenes/Example";
 
 const config = {
 	type: Phaser.AUTO,
@@ -10,26 +9,27 @@ const config = {
 	height: 600,
 	pixelArt: true,
 	physics: {
-		default: 'matter',
+		default: "arcade",
 		matter: {
 			gravity: { y: 9 },
-			debug: true,
+			debug: true
 		},
+		arcade: {
+			gravity: { y: 300 },
+			debug: false
+		}
 	},
-	scene: [
-		Example,
-	],
-}
+	scene: [Example]
+};
 
 // eslint-disable-next-line no-unused-vars
-const game = new Phaser.Game(config)
-
+const game = new Phaser.Game(config);
 
 // hot reloading - see if this works for games
 if (module.hot) {
-	module.hot.accept(() => {})
+	module.hot.accept(() => {});
 
 	module.hot.dispose(() => {
-		window.location.reload()
-	})
+		window.location.reload();
+	});
 }
--- src/scenes/Example.js
+++ src/scenes/Example.js
@@ -1,73 +1,89 @@
 // Import image files
-import _mountain from '../assets/images/mountain.png'
-import _platform from '../assets/images/platform.png'
-import _dude from '../assets/sprites/dude.png'
-
+import _mountain from "../assets/images/mountain.png";
+import _platform from "../assets/images/platform.png";
+import _dude from "../assets/sprites/dude.png";
 
 export default class Example extends Phaser.Scene {
-
 	constructor() {
 		// Name of my scene
-		super({ key: 'Example' })
+		super({ key: "Example" });
+		
 	}
 
 	preload() {
-		this.load.image('mountain', _mountain)
-		this.load.image('ground', _platform)
-		this.load.spritesheet('dude', _dude, { frameWidth: 32, frameHeight: 48 })
+		this.load.image("mountain", _mountain);
+		this.load.image("ground", _platform);
+		this.load.spritesheet("dude", _dude, {
+			frameWidth: 32,
+			frameHeight: 48
+		});
 	}
 
 	create() {
 		// background
-		this.add.image(400, 300, 'mountain')
+		this.add.image(400, 300, "mountain");
 
 		// platform
-		this.matter.add.image(400, 480, 'ground', null, { isStatic: true }).setAngle(13)
+		this._platform = this.physics.add.staticGroup();
+		this._platform.create(400, 480, "ground");
+
+		// this.matter = this.physics.add.staticGroup();
+		// this.matter.add
+		// 	.image(400, 480, "ground", null, { isStatic: true })
+		// 	.setAngle(0);
 
 		// player
-		this.player = this.matter.add.sprite(400, 400, 'dude')
-		this.player.setBounce(1)
+		this.player = this.physics.add.sprite(400, 400, "dude");
+		this.player.setBounce(0.2);
+		this.player.setCollideWorldBounds(true);
+		this.player.body.setGravityY(300);
 
 		this.anims.create({
-			key: 'left',
-			frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+			key: "left",
+			frames: this.anims.generateFrameNumbers("dude", {
+				start: 2,
+				end: 5
+			}),
 			frameRate: 10,
 			repeat: -1
-		})
+		});
 
 		this.anims.create({
-			key: 'turn',
-			frames: [ { key: 'dude', frame: 4 } ],
+			key: "turn",
+			frames: [{ key: "dude", frame: 4 }],
 			frameRate: 20
-		})
+		});
 
 		this.anims.create({
-			key: 'right',
-			frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+			key: "right",
+			frames: this.anims.generateFrameNumbers("dude", {
+				start: 2,
+				end: 5
+			}),
 			frameRate: 10,
 			repeat: -1
-		})
+		});
+		// adding the physics for the platforms
+		this.physics.add.collider(this.player, this._platform);
 
-		this.matter.world.setBounds().update30Hz()
-		this.cursors = this.input.keyboard.createCursorKeys()
+		// this.matter.world.setBounds().update30Hz();
+		this.cursors = this.input.keyboard.createCursorKeys();
 	}
 
 	update() {
-		if (this.cursors.left.isDown) {
-			this.player.setVelocityX(-1)
-			this.player.anims.play('left', true)
-
-		} else if (this.cursors.right.isDown) {
-			this.player.setVelocityX(1)
-			this.player.anims.play('right', true)
-
+		if (this.cursors.left.isDown && !this.player.body.touching.down) {
+			// this.player.setVelocityX(-160);
+			this.player.anims.play("left", true);
+		} else if (this.cursors.right.isDown && !this.player.body.touching.down) {
+			// this.player.setVelocityX(160);
+			this.player.anims.play("right", true);
 		} else {
-			this.player.setVelocityX(0)
-			this.player.anims.play('turn')
+			this.player.setVelocityX(0);
+			this.player.anims.play("turn");
 		}
 
-		if (this.cursors.up.isDown) {
-			this.player.setVelocityY(-6)
+		if (this.cursors.up.isDown && this.player.body.touching.down) {
+			this.player.setVelocityY(-300);
 		}
 	}
 }
