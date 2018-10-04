67903f12a1796bacd2a13f7b0e812a58faefbe51
Keung-Lee <43056649+Keung-Lee@users.noreply.github.com>
2018-10-04 14:26:26 +1000

    Added colour import

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,3 +1,4 @@
+import { INTERACTIVE_BUTTON, HEADINGS } from '../lib/constants'
 
 export default class MainMenu extends Phaser.Scene {
 
@@ -9,15 +10,15 @@ export default class MainMenu extends Phaser.Scene {
 	}
 
 	create() {
-		this.add.text(150, 100, 'Main Menu', {font: '80px Courier', fill: '#540F0F'})
+		this.add.text(150, 100, 'Main Menu', {font: '80px Courier', fill: HEADINGS})
 		// Play button
-		this.playButton = this.add.text(350, 250, 'Play', {font: '36px Courier', fill: '#466E85'})
+		this.playButton = this.add.text(350, 250, 'Play', {font: '36px Courier', fill: INTERACTIVE_BUTTON})
 		this.playButton.setInteractive()
 		this.playButton.on('pointerdown', () => {
 			this.scene.start('MainGame')
 		})
 
-		this.optionButton = this.add.text(350, 350, 'Options', {font: '36px Courier', fill: '#466E85'})
+		this.optionButton = this.add.text(350, 350, 'Options', {font: '36px Courier', fill: INTERACTIVE_BUTTON})
 	}
 
 	update() {
